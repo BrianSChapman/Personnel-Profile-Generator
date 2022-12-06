9eac0fb9fc8ac9bdb7df6dea191f225a8f9f5216
Brian Chapman <chapman.brian86@gmail.com>
2022-12-06 16:45:57 -0500

    Laid out the HTML skeleton to be dynamically generated

---
+++ JavaScript/index.js
@@ -0,0 +1,53 @@
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="jumbotron jumbotron-fluid bg-info">
+    <span class="border-bottom"></span>
+  <div class="container">
+    <h3 class="display-4 text-center">My Team</h3>
+</div>
+  </header>
+<main>
+    <div class="card-deck mt-5">
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+              
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item-1"></li>
+              <li class="list-group-item-2"></li>
+              <li class="list-group-item-3"></li>
+            </ul>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <div class="card-header">
+              
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item-1"></li>
+              <li class="list-group-item-2"></li>
+              <li class="list-group-item-3"></li>
+            </ul>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <div class="card-header">
+              
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item-1"></li>
+              <li class="list-group-item-2"></li>
+              <li class="list-group-item-3"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</main>
+
+</body>
+</html>`
