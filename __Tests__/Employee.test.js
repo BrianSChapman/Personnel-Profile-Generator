66ab8dd496297e0ebf9ccc5b3ed38431c4c0cfca
Brian Chapman <chapman.brian86@gmail.com>
2022-12-08 22:40:57 -0500

    Reworking tests to see if refactoring code is necessary

--- __Tests__/Employee.test.js
+++ __Tests__/Employee.test.js
@@ -1,9 +1,29 @@
-const { it, describe } = require("node:test");
 const Employee = require("../lib/employee");
 
 describe("Employee", () => {
-    describe("Initialization"), () => {
-        it("should return a constructor containing a  ")
-    }
+    describe("this.name",() => {
+    it("should return an object containing a 'name' property when called with the new keyword", () => {
+        const newName = "name";
+        expect(newName).toEqual("name"); 
+    })
+    })
+})
+    describe("this.id",() => {
+    it("should return an object containing a 'id' property when called with the new keyword", () => {
+        const newId = "id";
+        expect(newId).toEqual("id"); 
+    
+    })
 
 })
+
+describe("this.email",() => {
+    it("should return an object containing a 'email' property when called with the new keyword", () => {
+        const newEmail = "email";
+        expect(newEmail).toEqual("email"); 
+    
+    })
+
+})
+
+    
